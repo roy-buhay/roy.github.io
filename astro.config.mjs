0f2cd3f4cdb8f5add12202e85a822cb2e25b0fed
Roy Buhay <roydesign@outlook.com>
2024-07-23 12:25:57 +0800

    fix: assets config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 
 export default defineConfig({
     site: 'https://roy-buhay.github.io',
     base: 'roy-buhay',
+    image: {
+        service: passthroughImageService(),
+    },
     vite: {
         build: {
             assetsInclude: ['**/*.png','**/*.jpg','**/*.svg'],
